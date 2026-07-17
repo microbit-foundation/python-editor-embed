6f05eef8d28bcc2d3fa381307e577299f0894e99
Grace <grace@microbit.org>
2026-07-17 15:23:24 +0100

    Cludge around npm publish

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,6 +2,7 @@ import type { StorybookConfig } from '@storybook/react-vite';
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.stories.tsx'],
+  staticDirs: ['../public'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
